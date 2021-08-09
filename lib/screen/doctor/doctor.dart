f4a805e781f2e36e63007017ae47e6997b78e2fc
ardi7923 <ardiirawan777@gmail.com>
2021-08-10 01:04:53 +0800

    doctor list schedule done

--- lib/screen/doctor/doctor.dart
+++ lib/screen/doctor/doctor.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rumahsakit/layout/layout.dart';
-import 'package:flutter_rumahsakit/screen/consult/consult.dart';
 import 'package:flutter_rumahsakit/service/service.dart';
 import 'package:flutter_rumahsakit/shared/shared.dart';
 import 'package:flutter_rumahsakit/widget/widget.dart';
@@ -9,3 +8,4 @@ import 'package:get/get.dart';
 part "doctor_home_screen.dart";
 part "doctor_confirm_schedule_screen.dart";
 part "doctor_confirm_detail_screen.dart";
+part "doctor_schedule.dart";
--- lib/screen/doctor/doctor_home_screen.dart
+++ lib/screen/doctor/doctor_home_screen.dart
@@ -120,7 +120,7 @@ class _DoctorHomeScreenState extends State<DoctorHomeScreen> {
                               image: "assets/icons/add_schedule.png",
                               title: "Lihat Jadwal",
                               onTap: () {
-                                Get.to(() => ConsultDoctorScreen());
+                                Get.to(() => DoctorSchedule());
                               },
                             ),
                           ],
